53292ab849bcd40a3cde223a3c9cd40b9420f2e0
drbarq <J.Tustin@gmail.com>
2020-02-06 14:36:17 -0700

    Adding in first fetch

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -3,5 +3,15 @@ import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
 });
