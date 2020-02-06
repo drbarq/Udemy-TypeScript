8f9effa5c3df286c5df762a618f8c96162d316cd
drbarq <J.Tustin@gmail.com>
2020-02-06 16:20:38 -0700

    adding in annotation lesson

---
+++ annotations/variables.ts
@@ -0,0 +1,10 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+// variable called now that can only be assigned a type date
