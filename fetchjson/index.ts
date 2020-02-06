d66450576435097735d7b2120ddb7a032ee190d8
drbarq <J.Tustin@gmail.com>
2020-02-06 15:12:44 -0700

    learning types

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+// what ts should expect for the Todo object
 interface Todo {
   id: number;
   title: string;
@@ -15,9 +16,22 @@ axios.get(url).then(response => {
   const title = todo.title;
   const completed = todo.completed;
 
+  logTodo(id, title, completed);
+});
+
+// const logTodo = (id, title, completed) => {
+//   console.log(`
+//     The Todo with ID: ${id}
+//     Has a title of: ${title}
+//     Is it finished? ${completed}
+//   `);
+// };
+// clarifies the data type for the input
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
     The Todo with ID: ${id}
     Has a title of: ${title}
     Is it finished? ${completed}
   `);
-});
+};
