b19cd77bc11846d7b46270036d13b153d799924f
drbarq <J.Tustin@gmail.com>
2020-02-07 15:55:15 -0700

    using interfaces

---
+++ interfaces.ts
@@ -0,0 +1,37 @@
+interface Vehicle {
+	name: string;
+	year: Date;
+	broken: boolean;
+	summary(): string;
+}
+
+const oldCivic = {
+	name: 'civic',
+	year: new Date(),
+	broken: true,
+	summary(): string {
+		return `Name: ${this.name}`;
+	}
+};
+
+// const printVehicle = (vehicle: {
+// 	name: string;
+// 	year: number;
+// 	broken: boolean;
+// }): void => {
+// 	console.log(`Name: ${vehicle.name}`);
+// 	console.log(`Year: ${vehicle.year}`);
+// 	console.log(`Broken?: ${vehicle.broken}`);
+// };
+
+// using interface
+// const printVehicle = (vehicle: Vehicle): void => {
+// 	console.log(`Name: ${vehicle.name}`);
+// 	console.log(`Year: ${vehicle.year}`);
+// 	console.log(`Broken?: ${vehicle.broken}`);
+// };
+const printVehicle = (vehicle: Vehicle): void => {
+	console.log(`Name: ${vehicle.summary()}`);
+};
+
+printVehicle(oldCivic);
