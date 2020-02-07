b56f89cdb0baf13a6f1e48ae28c2772c332fe2b6
drbarq <J.Tustin@gmail.com>
2020-02-07 16:48:38 -0700

    classes

--- classes.ts
+++ classes.ts
@@ -1,7 +1,22 @@
+// class Vehicle {
+// 	// public drive(): void {
+// 	// 	console.log('chugga chugga');
+// 	// }
+
+// 	// color: string = 'red';
+// 	color: string;
+
+// 	constructor(color: string) {
+// 		this.color = color;
+// 	}
+
+// 	protected honk(): void {
+// 		console.log('beep');
+// 	}
+// }
+
 class Vehicle {
-	// public drive(): void {
-	// 	console.log('chugga chugga');
-	// }
+	constructor(public color: string) {}
 
 	protected honk(): void {
 		console.log('beep');
@@ -9,22 +24,25 @@ class Vehicle {
 }
 
 // const vehicle = new Vehicle();
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
+
 // vehicle.drive();
 // vehicle.honk();
 
 // allows us access to Vechicle class
-class Car extends Vehicle {
-	private drive(): void {
-		console.log('vroom');
-	}
+// class Car extends Vehicle {
+// 	private drive(): void {
+// 		console.log('vroom');
+// 	}
 
-	startDrivingProcess(): void {
-		this.drive();
-		this.honk();
-	}
-}
+// 	startDrivingProcess(): void {
+// 		this.drive();
+// 		this.honk();
+// 	}
+// }
 
-const car = new Car();
+// const car = new Car();
 
-car.startDrivingProcess();
+// car.startDrivingProcess();
 // car.honk();
