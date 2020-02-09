9a847ec8a6198769614f0cb3ff458d10e75a6b48
drbarq <J.Tustin@gmail.com>
2020-02-09 11:51:35 -0700

    adding in types

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -85168,7 +85168,9 @@ var Company_1 = require("./Company");
 
 var user = new User_1.User();
 var company = new Company_1.Company();
-console.log(user, company);
+console.log(user);
+console.log(company);
+google;
 },{"./User":"src/User.ts","./Company":"src/Company.ts"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -85197,7 +85199,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58501" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58628" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -4,4 +4,7 @@ import { Company } from './Company';
 const user = new User();
 const company = new Company();
 
-console.log(user, company);
+console.log(user);
+console.log(company);
+
+google;
