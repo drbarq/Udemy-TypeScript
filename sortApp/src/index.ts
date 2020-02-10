81d67276670ecef6a25a6e7c31d196e8d2a9a614
drbarq <J.Tustin@gmail.com>
2020-02-10 12:42:25 -0700

    Adding in sorting classes

--- sortApp/src/index.ts
+++ sortApp/src/index.ts
@@ -1,8 +1,17 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 50]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 50]);
+// const sorter = new Sorter(numbersCollection);
+
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('xisafeE');
+const sorter = new Sorter(charactersCollection);
 
 sorter.sort();
-console.log(numbersCollection.data);
+
+console.log(charactersCollection.data);
+console.log(sorter.collection);
