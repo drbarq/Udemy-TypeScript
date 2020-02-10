f1bf1f4f7e81e8cd8b17d0a800fdb7f5169c5ce1
drbarq <J.Tustin@gmail.com>
2020-02-10 12:20:17 -0700

    adding in sorting classes

--- sortApp/build/index.js
+++ sortApp/build/index.js
@@ -1,29 +1,8 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
-var sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+var sorter_1 = require("./sorter");
+var NumbersCollection_1 = require("./NumbersCollection");
+var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+var sorter = new sorter_1.Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
-// using public as the identifier is the same as the field definitition
-// class Sorter {
-//   collection: number[];
-//   constructor( collection: number[]) {
-//     this.collection = collection;
-//   }
-// }
+console.log(numbersCollection.data);
---
+++ sortApp/build/sorter.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection.compare(j, j + 1)) {
+                    this.collection.swap(j, j + 1);
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+exports.Sorter = Sorter;
+// export class Sorter {
+// 	constructor(public collection: ) {}
+// 	sort(): void {
+// 		const { length } = this.collection;
+// 		for (let i = 0; i < length; i++) {
+// 			for (let j = 0; j < length - i - 1; j++) {
+// 				// all of this only works if number[]
+// 				// if collection is an array of numbers
+// 				// type guard
+// 				if (this.collection instanceof Array) {
+// 					if (this.collection[j] > this.collection[j + 1]) {
+// 						const leftHand = this.collection[j];
+// 						this.collection[j] = this.collection[j + 1];
+// 						this.collection[j + 1] = leftHand;
+// 					}
+// 				}
+// 				if (typeof this.collection === 'string') {
+// 				}
+// 				// if it is a string, do this one instead
+// 				// only work if collection is string
+// 			}
+// 		}
+// 	}
+// }
