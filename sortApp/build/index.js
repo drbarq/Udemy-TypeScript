20293c7f5e750d7c1d666ce466ec638a1e800402
drbarq <J.Tustin@gmail.com>
2020-02-10 10:54:35 -0700

    adding in sort

--- sortApp/build/index.js
+++ sortApp/build/index.js
@@ -1,6 +1,29 @@
 "use strict";
-console.log('kljsflkjsldjkf');
-var logSomething = function () {
-    console.log('logged it');
-};
-logSomething();
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
+// using public as the identifier is the same as the field definitition
+// class Sorter {
+//   collection: number[];
+//   constructor( collection: number[]) {
+//     this.collection = collection;
+//   }
+// }
