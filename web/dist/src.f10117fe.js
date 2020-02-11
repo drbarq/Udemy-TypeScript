1ad15d07b6f3258fc261aedc00d262a16663dddc
drbarq <J.Tustin@gmail.com>
2020-02-11 16:57:39 -0700

    refactoring user with eventing

--- web/dist/src.f10117fe.js
+++ web/dist/src.f10117fe.js
@@ -1902,37 +1902,14 @@ var User =
 function () {
   function User(data) {
     this.data = data;
-    this.events = {};
   }
 
   User.prototype.get = function (propName) {
     return this.data[propName];
-  }; // set(update: { name: string; age: number }): void {}   // the same set below
-
+  };
 
   User.prototype.set = function (update) {
     Object.assign(this.data, update);
-  }; // callback is a function, takes no arguments and returns nothing
-  // on(eventName: string, callback: () => {}) {}
-
-
-  User.prototype.on = function (eventName, callback) {
-    // this.events[eventName]; //Callback[] or undefined
-    var handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  };
-
-  User.prototype.trigger = function (eventName) {
-    var handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach(function (callback) {
-      callback();
-    });
   };
 
   User.prototype.fetch = function () {
@@ -1959,7 +1936,64 @@ function () {
   return User;
 }();
 
-exports.User = User;
+exports.User = User; // pre refactor
+// import axios, { AxiosResponse } from 'axios';
+// interface UserProps {
+// 	id?: number;
+// 	name?: string;
+// 	age?: number;
+// 	// the ? makes the field optional
+// }
+// // WRONG - returns an object
+// // type Callback = () => {};
+// type Callback = () => void;
+// const usersUrl = 'http://localhost:3000/users';
+// export class User {
+// 	events: { [key: string]: Callback[] } = {};
+// 	constructor(private data: UserProps) {}
+// 	get(propName: string): string | number {
+// 		return this.data[propName];
+// 	}
+// 	// set(update: { name: string; age: number }): void {}   // the same set below
+// 	set(update: UserProps): void {
+// 		Object.assign(this.data, update);
+// 	}
+// 	// callback is a function, takes no arguments and returns nothing
+// 	// on(eventName: string, callback: () => {}) {}
+// 	on(eventName: string, callback: Callback): void {
+// 		// this.events[eventName]; //Callback[] or undefined
+// 		const handlers = this.events[eventName] || [];
+// 		handlers.push(callback);
+// 		this.events[eventName] = handlers;
+// 	}
+// 	trigger(eventName: string): void {
+// 		const handlers = this.events[eventName];
+// 		if (!handlers || handlers.length === 0) {
+// 			return;
+// 		}
+// 		handlers.forEach(callback => {
+// 			callback();
+// 		});
+// 	}
+// 	fetch(): void {
+// 		axios
+// 			// .get(`http://localhost:3000/users/${this.get('id')}`)
+// 			.get(`${usersUrl}/${this.get('id')}`)
+// 			.then((response: AxiosResponse): void => {
+// 				this.set(response.data);
+// 			});
+// 	}
+// 	save(): void {
+// 		const id = this.get('id');
+// 		if (id) {
+// 			// put
+// 			axios.put(`${usersUrl}/${id}`, this.data);
+// 		} else {
+// 			// post
+// 			axios.post(usersUrl, this.data);
+// 		}
+// 	}
+// }
 },{"axios":"node_modules/axios/index.js"}],"src/index.ts":[function(require,module,exports) {
 "use strict";
 
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -4,48 +4,19 @@ interface UserProps {
 	id?: number;
 	name?: string;
 	age?: number;
-	// the ? makes the field optional
 }
-
-// WRONG - returns an object
-// type Callback = () => {};
-type Callback = () => void;
 
 const usersUrl = 'http://localhost:3000/users';
 
 export class User {
-	events: { [key: string]: Callback[] } = {};
-
 	constructor(private data: UserProps) {}
 
 	get(propName: string): string | number {
 		return this.data[propName];
 	}
 
-	// set(update: { name: string; age: number }): void {}   // the same set below
 	set(update: UserProps): void {
 		Object.assign(this.data, update);
-	}
-
-	// callback is a function, takes no arguments and returns nothing
-	// on(eventName: string, callback: () => {}) {}
-	on(eventName: string, callback: Callback): void {
-		// this.events[eventName]; //Callback[] or undefined
-		const handlers = this.events[eventName] || [];
-		handlers.push(callback);
-		this.events[eventName] = handlers;
-	}
-
-	trigger(eventName: string): void {
-		const handlers = this.events[eventName];
-
-		if (!handlers || handlers.length === 0) {
-			return;
-		}
-
-		handlers.forEach(callback => {
-			callback();
-		});
 	}
 
 	fetch(): void {
@@ -69,3 +40,76 @@ export class User {
 		}
 	}
 }
+
+// pre refactor
+// import axios, { AxiosResponse } from 'axios';
+
+// interface UserProps {
+// 	id?: number;
+// 	name?: string;
+// 	age?: number;
+// 	// the ? makes the field optional
+// }
+
+// // WRONG - returns an object
+// // type Callback = () => {};
+// type Callback = () => void;
+
+// const usersUrl = 'http://localhost:3000/users';
+
+// export class User {
+// 	events: { [key: string]: Callback[] } = {};
+
+// 	constructor(private data: UserProps) {}
+
+// 	get(propName: string): string | number {
+// 		return this.data[propName];
+// 	}
+
+// 	// set(update: { name: string; age: number }): void {}   // the same set below
+// 	set(update: UserProps): void {
+// 		Object.assign(this.data, update);
+// 	}
+
+// 	// callback is a function, takes no arguments and returns nothing
+// 	// on(eventName: string, callback: () => {}) {}
+// 	on(eventName: string, callback: Callback): void {
+// 		// this.events[eventName]; //Callback[] or undefined
+// 		const handlers = this.events[eventName] || [];
+// 		handlers.push(callback);
+// 		this.events[eventName] = handlers;
+// 	}
+
+// 	trigger(eventName: string): void {
+// 		const handlers = this.events[eventName];
+
+// 		if (!handlers || handlers.length === 0) {
+// 			return;
+// 		}
+
+// 		handlers.forEach(callback => {
+// 			callback();
+// 		});
+// 	}
+
+// 	fetch(): void {
+// 		axios
+// 			// .get(`http://localhost:3000/users/${this.get('id')}`)
+// 			.get(`${usersUrl}/${this.get('id')}`)
+// 			.then((response: AxiosResponse): void => {
+// 				this.set(response.data);
+// 			});
+// 	}
+
+// 	save(): void {
+// 		const id = this.get('id');
+
+// 		if (id) {
+// 			// put
+// 			axios.put(`${usersUrl}/${id}`, this.data);
+// 		} else {
+// 			// post
+// 			axios.post(usersUrl, this.data);
+// 		}
+// 	}
+// }
