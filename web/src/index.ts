30acffe4c576cbef62aa485370f5e9ad370fbcce
drbarq <J.Tustin@gmail.com>
2020-02-11 15:12:59 -0700

    adding in axios and db

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,16 +1,25 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'Joe', age: 20 });
+// axios.post('http://localhost:3000/users', {
+// 	name: 'my name',
+// 	age: 20
+// });
 
-user.on('change', () => {
-	console.log('Change #1');
-});
-user.on('change', () => {
-	console.log('Change #2');
-});
-user.on('save', () => {
-	console.log('Save was triggered');
-});
+axios.get('http://localhost:3000/users/1');
 
-user.trigger('asdfdsf');
-console.log(user);
+// import { User } from './models/User';
+
+// const user = new User({ name: 'Joe', age: 20 });
+
+// user.on('change', () => {
+// 	console.log('Change #1');
+// });
+// user.on('change', () => {
+// 	console.log('Change #2');
+// });
+// user.on('save', () => {
+// 	console.log('Save was triggered');
+// });
+
+// user.trigger('asdfdsf');
+// console.log(user);
