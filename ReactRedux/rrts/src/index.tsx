4139f51553a40e2b62680b67524a01edb68f54db
drbarq <J.Tustin@gmail.com>
2020-02-17 12:09:03 -0700

    adding in reducers and redux

--- ReactRedux/rrts/src/index.tsx
+++ ReactRedux/rrts/src/index.tsx
@@ -4,11 +4,14 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
+import { App } from './components/App';
+import { reducers } from './reducers/index';
+
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDom.render(
 	<Provider store={store}>
-		<App color="red" />
+		<App />
 	</Provider>,
 	document.querySelector('#root')
 );
