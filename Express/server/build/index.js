643359d38cba0c353f4c74c00f783bf663c4c060
drbarq <J.Tustin@gmail.com>
2020-02-17 16:30:38 -0700

    adding in router

--- Express/server/build/index.js
+++ Express/server/build/index.js
@@ -4,10 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var loginRoutes_1 = require("./routes/loginRoutes");
 var app = express_1.default();
-app.get('/', function (req, res) {
-    res.send("\n    <div>\n      <h1>Hi There!</h1>\n    </div>\n  ");
-});
+app.use(loginRoutes_1.router);
 app.listen(3000, function () {
     console.log('listening on port 3000');
 });
+// app.get('/', (req: Request, res: Response) => {
+// 	res.send(`
+//     <div>
+//       <h1>Hi There!</h1>
+//     </div>
+//   `);
+// });
--- Express/server/src/index.ts
+++ Express/server/src/index.ts
@@ -1,15 +1,18 @@
 import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-	res.send(`
-    <div>
-      <h1>Hi There!</h1>
-    </div>
-  `);
-});
+app.use(router);
 
 app.listen(3000, () => {
 	console.log('listening on port 3000');
 });
+
+// app.get('/', (req: Request, res: Response) => {
+// 	res.send(`
+//     <div>
+//       <h1>Hi There!</h1>
+//     </div>
+//   `);
+// });
