0be6a141d8f79201ceaab5c733c2a4a96698b917
drbarq <J.Tustin@gmail.com>
2020-02-17 14:13:54 -0700

    adding in delete

--- ReactRedux/rrts/src/components/App.tsx
+++ ReactRedux/rrts/src/components/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Todo, fetchTodos } from '../actions';
+import { Todo, fetchTodos, deleteTodo } from '../actions';
 import { StoreState } from '../reducers';
 
 interface AppProps {
 	todos: Todo[];
-	fetchTodos(): any;
+	fetchTodos: typeof fetchTodos;
+	deleteTodo: typeof deleteTodo;
 }
 
 class _App extends React.Component<AppProps> {
@@ -13,9 +14,17 @@ class _App extends React.Component<AppProps> {
 		this.props.fetchTodos();
 	};
 
+	onTodoClick = (id: number): void => {
+		this.props.deleteTodo(id);
+	};
+
 	renderList(): JSX.Element[] {
 		return this.props.todos.map((todo: Todo) => {
-			return <div key={todo.id}>{todo.title}</div>;
+			return (
+				<div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>
+					{todo.title}
+				</div>
+			);
 		});
 	}
 
@@ -40,4 +49,4 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
 
 // we dont want to export App we want to export connected App
 // first (configuration)(_App)
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
--- ReactRedux/rrts/src/reducers/todos.ts
+++ ReactRedux/rrts/src/reducers/todos.ts
@@ -2,9 +2,12 @@ import { Todo, Action, ActionTypes } from '../actions';
 
 // state will be an array of todos and if nothing is provided, just use an empty array
 export const todosReducer = (state: Todo[] = [], action: Action) => {
+	// the switch statement acts as a type guard
 	switch (action.type) {
 		case ActionTypes.fetchTodos:
 			return action.payload;
+		case ActionTypes.deleteTodo:
+			return state.filter((todo: Todo) => todo.id !== action.payload);
 		default:
 			return state;
 	}
