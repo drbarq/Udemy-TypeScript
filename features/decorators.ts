78d97c2e7e64d77e9a008068ca074d0c4dd2e7e7
drbarq <J.Tustin@gmail.com>
2020-02-18 16:41:28 -0700

    adding in decorator factory

--- features/decorators.ts
+++ features/decorators.ts
@@ -3,19 +3,58 @@ class Boat {
 	color: string = 'red';
 
 	// accessor
-	// @testDecorator
 	get formattedColor(): string {
 		return `This boat's color is ${this.color}`;
 	}
 
 	// method
-	@testDecorator
+	// @logError
+	@logError('Oops, boat was sunk in ocean') //pass in a config elements into logError
 	pilot(): void {
+		throw new Error();
 		console.log('swish');
 	}
 }
 
-function testDecorator(target: any, key: string): void {
-	console.log('Target:', target); // target is the prototype of Boat
-	console.log('key:', key); // key of the property/accessor/method it is placed on
+// decorator factory
+function logError(errorMessage: string) {
+	return function(target: any, key: string, desc: PropertyDescriptor): void {
+		const method = desc.value;
+
+		desc.value = function() {
+			try {
+				method();
+			} catch (e) {
+				console.log(errorMessage);
+			}
+		};
+	};
 }
+
+new Boat().pilot();
+
+// decorator examples
+// class Boat {
+// 	// property
+// 	color: string = 'red';
+
+// 	// accessor
+// 	// @testDecorator
+// 	get formattedColor(): string {
+// 		return `This boat's color is ${this.color}`;
+// 	}
+
+// 	// method
+// 	// @testDecorator
+// 	pilot(): void {
+// 		console.log('swish');
+// 	}
+// }
+
+// function testDecorator(target: any, key: string): void {
+// 	console.log('Target:', target); // target is the prototype of Boat
+// 	console.log('key:', key); // key of the property/accessor/method it is placed on
+// }
+
+// // testDecorator(Boat.prototype, 'pilot');
+// // same as @testDecorator above pilot()
