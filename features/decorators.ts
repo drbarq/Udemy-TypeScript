fd934aeef92c0b9bbb38ad1935aff34fc54d4f7b
drbarq <J.Tustin@gmail.com>
2020-02-18 15:20:27 -0700

    an decorator is run once when the class is defined

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,10 +1,14 @@
 class Boat {
+	// property
 	color: string = 'red';
 
+	// accessor
+	// @testDecorator
 	get formattedColor(): string {
 		return `This boat's color is ${this.color}`;
 	}
 
+	// method
 	@testDecorator
 	pilot(): void {
 		console.log('swish');
@@ -12,6 +16,6 @@ class Boat {
 }
 
 function testDecorator(target: any, key: string): void {
-	console.log('Target:', target);
-	console.log('key:', key);
+	console.log('Target:', target); // target is the prototype of Boat
+	console.log('key:', key); // key of the property/accessor/method it is placed on
 }
