76ccc76ffd140fcd8fa938cc4d18ad628f8157dd
drbarq <J.Tustin@gmail.com>
2020-02-18 17:17:02 -0700

    adding in class decorators

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,24 +1,49 @@
+@classDecorator
 class Boat {
 	@testDecorator
 	// property
 	color: string = 'red';
 
+	@testDecorator
 	// accessor
 	get formattedColor(): string {
 		return `This boat's color is ${this.color}`;
 	}
 
+	// // method
+	// // @logError
+	// @logError('Oops, boat was sunk in ocean') //pass in a config elements into logError
+	// pilot(): void {
+	// 	throw new Error();
+	// 	console.log('swish');
+	// }
+
 	// method
-	// @logError
-	@logError('Oops, boat was sunk in ocean') //pass in a config elements into logError
-	pilot(): void {
-		throw new Error();
-		console.log('swish');
+	@logError('Oops, boat was sunk in ocean')
+	pilot(
+		@parameterDecorator speed: string,
+		@parameterDecorator wake: boolean
+	): void {
+		if (speed === 'fast') {
+			console.log('swish');
+		} else {
+			console.log('nothing');
+		}
 	}
 }
 
+// apply a class decorator we only pass in constructor
+function classDecorator(constructor: typeof Boat) {
+	console.log(constructor);
+}
+
+// adding a decorators to the param/argument
+function parameterDecorator(target: Boat, key: string, index: number) {
+	console.log(key, index);
+}
+
 function testDecorator(target: any, key: string) {
-	console.log(target);
+	// console.log(target);
 	console.log(key);
 }
 
